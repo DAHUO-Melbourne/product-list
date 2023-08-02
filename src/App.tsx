b2b4217363f6b1bf435e2aeace1b41e46f45a1c4
DAHUO-Melbourne <huodamelbourne@gmail.com>
2023-08-02 20:52:38 +1000

    feat: add product items into shopping cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import data from './constants/products.json';
 
 const App = () => {
   const products = data.products;
+
+  const [shoppingItemList, setShoppingItemList] = useState<string[]>([]);
+
+  const handleProductClick = (e: React.MouseEvent) => {
+    const selectedCard = e.target as HTMLElement;
+    if(selectedCard.nodeName !== 'BUTTON') {
+      return;
+    }
+    const selectedProductLabel = selectedCard.closest('div')?.ariaLabel as string;
+    setShoppingItemList([
+      ...shoppingItemList,
+      selectedProductLabel
+    ])
+  }
+
   return (
     <div className="App">
-      {products.map((item) => (
-        <div
-          key={item.name}
-          className='product-item'
-        >
-          <h1>
-            {item.name}
-          </h1>
-          <h3>
-            {item.type}
-          </h3>
-          <h5>
-            {item.price}
-          </h5>
-        </div>
-      ))}
+      <div onClick={handleProductClick}>
+        {products.map((item) => (
+          <div
+            key={item.name}
+            className='product-item'
+            aria-label={item.name}
+          >
+            <h1>
+              {item.name}
+            </h1>
+            <h3>
+              {item.type}
+            </h3>
+            <h5>
+              {item.price}
+            </h5>
+            <button>
+              +
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
